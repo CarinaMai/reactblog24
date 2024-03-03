341432dfd9d815f3ad0bc65a6d5f698163dbf773
CarinaMai <130506848+CarinaMai@users.noreply.github.com>
2024-03-03 23:01:40 +0100

    Neu mit leeren Unterseiten

---
+++ src/Europa.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Europa() {
+  return (
+    <div>
+      <h2>Europa</h2>
+      {/* Hier kannst du den Inhalt für die Europa-Seite einfügen */}
+    </div>
+  );
+}
+
+export default Europa;
